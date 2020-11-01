e11e733c41ce8729c7ce93a5643cf7511f5f4ad2
Gor <gorsargsyanm@gmail.com>
2020-11-01 21:17:23 +0400

    add dict_exercises

--- Python Exercises/Dict_exercises/solution.py
+++ Python Exercises/Dict_exercises/solution.py
@@ -29,7 +29,7 @@ sampleDict = {
       }
    }
 }
-print(sampleDict["class"])
+
 def dict_func_3(dict1,name):
     for i in dict1.values():
         if type(i) == dict:
@@ -41,3 +41,75 @@ def dict_func_3(dict1,name):
                         
 
 dict_func_3(sampleDict,"history")
+
+#Question 4
+def dict_func_4(arr:list , dict1:dict):
+    return {i:dict1 for i in arr}
+
+employees = ['Kelly', 'Emma', 'John']
+defaults = {"designation": 'Application Developer', "salary": 8000}
+
+print(dict_func_4(employees,defaults))
+
+
+#Question 5
+def dict_func_5(arr:list,dict1:dict):
+    return {i:dict1[i] for i in arr}
+
+sampleDict = { "name": "Kelly", "age":25,"salary": 8000,"city": "New york" }
+key_list = ["name","salary"]
+
+print(dict_func_5(key_list,sampleDict))
+
+#Question 6
+def dict_func_6(arr:list,dict1:dict):
+    return {i:dict1[i] for i in dict1.keys() if not(i in arr)}
+
+sampleDict = { "name": "Kelly", "age":25,"salary": 8000,"city": "New york" }
+key_list = ["name","salary"]
+
+print(dict_func_6(key_list,sampleDict))
+
+#Question 7
+def dict_func_7(dict1:dict,value):
+    return  value in dict1.values()
+
+sampleDict = {'a': 100, 'b': 200, 'c': 300}
+print(dict_func_7(sampleDict,200))
+    
+
+#Question 8
+def dict_func_8(dict1:dict,key,rename_key):
+    dict1[rename_key] = dict1.pop(key)
+    return dict1
+sampleDict = { "name": "Kelly", "age":25,"salary": 8000,"city": "New york" }
+
+print(dict_func_8(sampleDict,"city","location"))
+
+#Question 9
+def dict_func_9(dict1:dict):
+    for key in dict1.keys():
+        if dict1[key] == min(dict1.values()):
+            return key
+
+sampleDict = {
+  'Physics': 82,
+  'Math': 65,
+  'history': 75
+}
+
+print(dict_func_9(sampleDict))
+
+#Question 10
+def dict_func_10(dict1:dict,name:str,changed_salary:int):
+    for key in dict1.keys():
+        if name in dict1[key].values():
+            if "salary" in dict1[key].keys():
+                dict1[key]["salary"] = changed_salary
+    return dict1
+sampleDict = {
+     'emp1': {'name': 'Jhon', 'salary': 7500},
+     'emp2': {'name': 'Emma', 'salary': 8000},
+     'emp3': {'name': 'Brad', 'salary': 6500}
+}
+print(dict_func_10(sampleDict,"Brad",150000000))
