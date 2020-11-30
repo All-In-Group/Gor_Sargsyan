472c46cb624f6cda7cf41972866337f2e64d4fc4
Gor <gorsargsyanm@gmail.com>
2020-11-30 21:53:26 +0400

    deleted to do

--- To do/src/tasks/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index , name = "list"),
-    path('update_task/<str:prk>',views.update_task, name = "update_task"),
-    path('delete/<str:prk>',views.delete_task, name = "delete"),
-]
